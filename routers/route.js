217faff270687570990460f2cf5c4d29b66849c1
Denver <70090559+Denver44@users.noreply.github.com>
2022-06-17 07:10:22 +0530

    clean up done

--- routers/route.js
+++ routers/route.js
@@ -1,7 +1,4 @@
-import tourRouter from './tourRoutes.js';
 import userRouter from './userRoutes.js';
-import reviewRouter from './reviewRoutes.js';
-import viewRouter from './viewRoutes.js';
-import bookingRouter from './bookingRouter.js';
 
-export { tourRouter, userRouter, reviewRouter, viewRouter, bookingRouter };
+// eslint-disable-next-line import/prefer-default-export
+export { userRouter };
